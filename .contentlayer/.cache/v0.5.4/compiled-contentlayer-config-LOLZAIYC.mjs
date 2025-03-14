0ecaafb16824d1d0370b9060d3716a26e4c43003
Leo Constantin <lcon69184@gmail.com>
2025-03-14 11:59:49 -0700

    added blog page

---
+++ .contentlayer/.cache/v0.5.4/compiled-contentlayer-config-LOLZAIYC.mjs
@@ -0,0 +1,85 @@
+// contentlayer.config.ts
+import { defineDocumentType, makeSource } from "contentlayer2/source-files";
+var Post = defineDocumentType(() => ({
+  name: "Post",
+  filePathPattern: `blog/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true
+    },
+    description: {
+      type: "string"
+    },
+    date: {
+      type: "date",
+      required: true
+    },
+    published: {
+      type: "boolean",
+      default: true
+    },
+    image: {
+      type: "string",
+      required: true
+    },
+    authors: {
+      type: "list",
+      of: { type: "string" },
+      required: true
+    }
+  },
+  computedFields: {
+    slug: {
+      type: "string",
+      resolve: (doc) => `/${doc._raw.flattenedPath}`
+    },
+    slugAsParams: {
+      type: "string",
+      resolve: (doc) => doc._raw.flattenedPath.split("/").slice(1).join("/")
+    }
+  }
+}));
+var Author = defineDocumentType(() => ({
+  name: "Author",
+  filePathPattern: `authors/**/*.mdx`,
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true
+    },
+    description: {
+      type: "string"
+    },
+    avatar: {
+      type: "string",
+      required: true
+    },
+    twitter: {
+      type: "string",
+      required: true
+    }
+  },
+  computedFields: {
+    slug: {
+      type: "string",
+      resolve: (doc) => `/${doc._raw.flattenedPath}`
+    },
+    slugAsParams: {
+      type: "string",
+      resolve: (doc) => doc._raw.flattenedPath.split("/").slice(1).join("/")
+    }
+  }
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "./content",
+  documentTypes: [Post, Author]
+});
+export {
+  Author,
+  Post,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-LOLZAIYC.mjs.map
