8919e9fbbd0e428fb9114ad36d7c1c712bf231c1
Leo Constantin <lcon69184@gmail.com>
2025-03-14 11:21:37 -0700

    finished home section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,85 @@
+/* eslint-disable react/no-unescaped-entities */
+import { GitHubIcon, LinkedInIcon, TwitterIcon } from "@/components/icons/social";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="mx-5 sm:mx-0 flex flex-col space-y-5 max-w-xl">
+      
+      <Image
+        src="/profile.jpeg"
+        alt="Profile Picture"
+        width={60}
+        height={60}
+        className="rounded-full size-16"
+      />
+      <h1 className="text-5xl font-bold tracking-tighter">
+        Programmer. Writer. Pianist.
+      </h1>
+      <p className="mt-6 text-base text-zinc-600">
+        Hey, I'm Steven. I'm the founder and CEO of{" "}
+        <a
+          href="https://dub.co"
+          target="_blank"
+          className="underline underline-offset-4 text-zinc-800 font-semibold"
+        >
+          Dub
+        </a>{" "}
+        – the open source link management infrastructure for modern marketing
+        teams.
+      </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <p className="mt-6 text-base text-zinc-600">
+        Previously, I helped build the future of the web at{" "}
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://vercel.com/"
           target="_blank"
-          rel="noopener noreferrer"
+          className="underline underline-offset-4 text-zinc-800 font-semibold"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+          Vercel
+        </a>{" "}
+        . I also created projects like{" "}
+        <a
+          href="https://sharegpt.com"
+          target="_blank"
+          className="underline underline-offset-4 text-zinc-800 font-semibold"
+        >
+          ShareGPT
+        </a>
+        ,{" "}
+        <a
+          href="https://novel.sh/"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="underline underline-offset-4 text-zinc-800 font-semibold"
+        >
+          Novel
+        </a>
+        , and{" "}
+        <a
+          href="https://oneword.domains"
+          target="_blank"
+          className="underline underline-offset-4 text-zinc-800 font-semibold"
+        >
+          One Word Domains
+        </a>
+        .
+      </p>
+      <div className="mt-6 flex gap-6">
+        <a href="https://twitter.com/steventey" target="_blank">
+          <TwitterIcon className="h-6 w-6 hover:scale-105 transition-all" />
+        </a>
+        <a href="https://github.com/steven-tey" target="_blank">
+          <GitHubIcon className="h-6 w-6 hover:scale-105 transition-all" />
         </a>
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://linkedin.com/in/steventey"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer noopener"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
+          <LinkedInIcon className="h-6 w-6 hover:scale-105 transition-all" />
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    
+      fontFamily: {
+        sans: ["var(--font-geist-sans)"],
+        mono: ["var(--font-geist-mono)"],
       },
     },
   },
