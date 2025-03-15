197145dac6f78ecc0be9f4c8ff38d8a22c6b6660
Leo Constantin <lcon69184@gmail.com>
2025-03-15 01:12:03 -0700

    changed link

--- src/app/blog/_components/NewsletterForm .tsx
+++ src/app/blog/_components/NewsletterForm .tsx
@@ -22,7 +22,7 @@ export default function NewsletterForm() {
           </p>
         </div>
         <iframe
-          data-tally-src="https://tally.so/embed/nG5gez?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+          data-tally-src="https://tally.so/embed/3xvoV9?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
           loading="lazy"
           width="100%"
           height="224"
